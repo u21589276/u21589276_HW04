b84ce027cf3602566c2a92f53ef89eedea481d9d
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-08-07 22:25:23 +0200

    styles and frm updated

---
+++ u21589276_HW04/wwwroot/js/Registration.js
@@ -0,0 +1,28 @@
+﻿try {
+    document.getElementById("userTypeInput").addEventListener('change', function () {
+        checkUserType();
+    });
+}
+catch {
+    //catch statement goes here
+}
+
+
+function checkUserType() {
+    var selectedUser = document.getElementById("userTypeInput").value;
+    var individualUserfrm = document.getElementById("indiUserfrm");
+    var OrganisationUserfrm = document.getElementById("orgUserfrm");
+
+    if (selectedUser == "Individual") {
+        individualUserfrm.style.display = "block";
+        OrganisationUserfrm.style.display = "none";
+    }
+    else if (selectedUser == "Organisation/Company") {
+        individualUserfrm.style.display = "none";
+        OrganisationUserfrm.style.display = "block";
+    }
+    else {
+        individualUserfrm.style.display = "none";
+        OrganisationUserfrm.style.display = "none";
+	}
+}
