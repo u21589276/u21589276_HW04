dfc0c739f9347258240f0c8974e6359832388880
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-08-19 23:46:41 +0200

    donation type selection

--- u21589276_HW04/wwwroot/js/site.js
+++ u21589276_HW04/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var Clothingfrmcontrol = document.getElementById("clothingCat");
+var Foodfrmcontrol = document.getElementById("FoodCat");
+
+try {
+    document.getElementById("CategoryInput").addEventListener('change', function () {
+        checkCategoryType();
+    });
+}
+catch {
+    //
+}
+
+function checkCategoryType() {
+    var selectedCategory = document.getElementById("CategoryInput").value;
+    if (selectedCategory == "Food") {
+        Clothingfrmcontrol.style.display = "none";
+        Foodfrmcontrol.style.display = "block";
+    }
+    else if (selectedCategory == "Clothing") {
+        Clothingfrmcontrol.style.display = "block";
+        Foodfrmcontrol.style.display = "none";
+    }
+}
